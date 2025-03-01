92f1de83413e0e23de9ea766a55275515032ec45
furoku <flock.h@gmail.com>
2025-03-01 19:51:47 +0900

    Prismaクライアントを追加

---
+++ src/lib/prisma/client.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+// PrismaClientを一度だけインスタンス化するためのシングルトンパターン
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
